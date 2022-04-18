b18a4fa6874d63d4202381b9fb5d0ffc62f23b93
Russell Banks <Russell@bandev.uk>
2022-04-19 00:58:43 +0100

    Add list of which ports are open

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,29 @@
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.prompt
 import java.net.InetAddress
 import java.net.Socket
 
 class Skan : CliktCommand() {
-    private val host: String by option(help="The hostname to connect to").default("localhost")
+    private val host: String by option("-h", "--hostname", help="The hostname to connect to").prompt("Hostname")
 
     override fun run() {
         val inetAddress = InetAddress.getByName(host)
         val hostName = inetAddress.hostName
+        val listOfOpenPorts = mutableListOf<Int>()
         for (port in 0..65535) {
             runCatching {
                 Socket(hostName, port).run {
                     echo("Port $port is open on $hostName")
+                    listOfOpenPorts.add(port)
                     close()
                 }
             }
+        }
+        listOfOpenPorts.run {
+            val last = last()
+            remove(last())
+            println(joinToString(prefix = "Ports ", postfix = " and $last are open on $hostName"))
         }
     }
 }
