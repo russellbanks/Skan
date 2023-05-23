673461cd150c6479f78f56f360a63ff1d4ab15e7
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2023-05-23 10:26:38 +0100

    Add Kotlin plugin to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.21"
+    alias(libs.plugins.kotlin)
     application
 }
 
